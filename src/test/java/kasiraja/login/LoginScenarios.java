a40474ee7584a8cd1ab1e90ee6436d71e872f6c5
d1m4zn4nd4 <dixdam16@gmail.com>
2023-10-22 11:50:12 +0700

    Refactor Java Class into LoginScenarios Adding SelventureWrongPassword

--- src/test/java/kasiraja/login/LoginScenarios.java
+++ src/test/java/kasiraja/login/LoginScenarios.java
@@ -12,12 +12,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class SuccessfullLogin {
+public class LoginScenarios {
     @Test
     public void SelVentureLogin(){
         WebDriver driver;
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        Duration timeout = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver,timeout);
         driver.manage().window().maximize();
 
         String baseUrl = "https://kasirdemo.belajarqa.com/";
@@ -34,12 +36,40 @@ public class SuccessfullLogin {
         passwordinput.sendKeys("test321");
         loginbutton.click();
 
-        Duration timeout = Duration.ofSeconds(10);
-        WebDriverWait wait = new WebDriverWait(driver,timeout);
         WebElement userfullname = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//dt[normalize-space()='Sel Ventures']")));
         String loginname = userfullname.getText();
         System.out.println(loginname);
         Assert.assertEquals("Sel Ventures",loginname);
+
+        driver.quit();
+    }
+
+    @Test
+    public void SelVentureWrongPassword(){
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        Duration timeout = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver,timeout);
+        driver.manage().window().maximize();
+
+        String baseUrl = "https://kasirdemo.belajarqa.com/";
+        driver.get(baseUrl);
+        String Url = driver.getCurrentUrl();
+        System.out.println(Url);
+        Assert.assertEquals("https://kasirdemo.belajarqa.com/login",Url);
+
+        WebElement logininput = driver.findElement(By.xpath("//input[@id='email']"));
+        WebElement passwordinput = driver.findElement(By.xpath("//input[@id='password']"));
+        WebElement loginbutton = driver.findElement(By.xpath("//button[normalize-space()='login']"));
+
+        logininput.sendKeys("admin@sel.com");
+        passwordinput.sendKeys("321");
+        loginbutton.click();
+
+        WebElement AlertKredensial = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@role='alert']")));
+        String AlertText = AlertKredensial.getText();
+        Assert.assertEquals("Kredensial yang Anda berikan salah", AlertText);
 
         driver.quit();
     }
